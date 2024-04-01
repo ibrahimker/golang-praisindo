f85e26dcc62894c14bf94b3093b12bb97e6ca404
Ibrahim Nurandita Isbandiputra <ibrahim.nurandita@wartek.belajar.id>
2024-04-02 03:11:38 +0700

    user service http with database connection

--- user-service-http/repository/user_repository.go
+++ user-service-http/repository/user_repository.go
@@ -1,13 +1,14 @@
 package repository
 
 import (
-	"errors"
+	"database/sql"
+	"log"
 
 	"github.com/ibrahimker/golang-praisindo/user-service-http/entity"
 )
 
 type UserRepository struct {
-	userDB []entity.User
+	db *sql.DB
 }
 
 type IUserRepository interface {
@@ -18,56 +19,82 @@ type IUserRepository interface {
 	Delete(email string) error
 }
 
-func NewUserRepository(userDB []entity.User) IUserRepository {
+func NewUserRepository(db *sql.DB) IUserRepository {
 	return &UserRepository{
-		userDB: userDB,
+		db: db,
 	}
 }
 
 func (u *UserRepository) GetAll() ([]entity.User, error) {
-	return u.userDB, nil
+	queryString := "SELECT username, email, password, age FROM users LIMIT 10"
+	rows, err := u.db.Query(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var res []entity.User
+	for rows.Next() {
+		var user entity.User
+		if err := rows.Scan(
+			&user.Username, &user.Email, &user.Password, &user.Age,
+		); err != nil {
+			log.Println(err)
+			continue
+		}
+		res = append(res, user)
+	}
+
+	return res, nil
 }
 
 func (u *UserRepository) GetByEmail(email string) (entity.User, error) {
-	for _, user := range u.userDB {
-		if user.Email == email {
-			return user, nil
-		}
+	var user entity.User
+	queryString := "SELECT username, email, password, age FROM users WHERE email = $1 LIMIT 1"
+	if err := u.db.QueryRow(queryString, email).Scan(
+		&user.Username, &user.Email, &user.Password, &user.Age); err != nil {
+		log.Println(err)
+		return entity.User{}, err
 	}
-	return entity.User{}, errors.New("user not found")
+
+	return user, nil
 }
 
 // Create implements IUserRepository.
 func (u *UserRepository) Create(user entity.User) (entity.User, error) {
-	u.userDB = append(u.userDB, user)
+	queryString := "INSERT INTO users (username, email, password, age) " +
+		"VALUES ($1,$2,$3,$4) "
+	_, err := u.db.Exec(queryString, user.Username, user.Email, user.Password, user.Age)
+	if err != nil {
+		log.Println(err)
+		return user, err
+	}
+
 	return user, nil
 }
 
 // Delete implements IUserRepository.
 func (u *UserRepository) Delete(email string) error {
-	// copy all element to new slice except element that want to be deleted
-	var userCopy []entity.User
-	for _, user := range u.userDB {
-		if user.Email != email {
-			userCopy = append(userCopy, user)
-		}
+	queryString := "DELETE FROM users WHERE email = $1"
+	_, err := u.db.Exec(queryString, email)
+	if err != nil {
+		log.Println(err)
+		return err
 	}
-	u.userDB = userCopy
 
 	return nil
 }
 
 // Update implements IUserRepository.
 func (u *UserRepository) Update(user entity.User) (entity.User, error) {
-	// naive approach to update is by delete the data first then create the data again
-	if err := u.Delete(user.Email); err != nil {
-		return entity.User{}, err
-	}
-
-	newUser, err := u.Create(user)
+	queryString := "UPDATE users " +
+		"SET username=$2, password=$3, age=$4" +
+		"WHERE email=$1"
+	_, err := u.db.Exec(queryString, user.Email, user.Username, user.Password, user.Age)
 	if err != nil {
-		return entity.User{}, err
+		log.Println(err)
+		return user, err
 	}
 
-	return newUser, nil
+	return user, nil
 }
